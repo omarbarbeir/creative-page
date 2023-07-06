8273d68c20d03faaec65f5c0ed44675ebb8bc113
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-07-06 20:23:54 +0300

    Add files via upload

---
+++ creative page.js
@@ -0,0 +1,14 @@
+let image = document.getElementById('image')
+let container = document.getElementsByClassName('container')[0]
+
+function ChangePhone(phone)
+{
+    image.src = phone;
+}
+
+function Colors(color)
+{
+    container.style.background = color;
+}
+
+
